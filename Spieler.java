6168cae2b1f414d5263f385be93c077ad1b04999
Alexander Bartl <bartlal61570@th-nuernberg.de>
2019-04-15 13:47:39 +0200

    Changed card generation
    All cards get generated by class Bierkopf instead of Spieler

--- Spieler.java
+++ Spieler.java
@@ -2,24 +2,11 @@ package Bierkopf;
 
 public class Spieler {
 
-  Karte[] all_cards;
+  Karte[] handkarten;
 
   public Spieler() {
-    // Problem hierbei: Jeder Spieler würde nun "all_cards" generieren. 
-    // Vllt sollte das lieber nur die "Bierkopf-Klasse" tun. 
-    all_cards = new Karte[24];
+    System.out.println("Konstruktor: Spieler()");
 
-    // initialize all cards
-    int i = 0;
-    for (FARBE f : FARBE.values())
-      for (ZAHL z : ZAHL.values())
-        all_cards[i++] = new Karte(z, f);
-
-    // print all cards + trump
-    for (i = 0; i < 24; i++) {
-      System.out.print(all_cards[i].get_karte() + " ");
-      System.out.println(all_cards[i].get_trumpf());
-    }
   }
 
 }
